fc6c3b6c70bb61870990832fb25db258d314f398
99sparshm <99.sparsh@gmail.com>
2020-06-22 19:47:54 +0530

    first

---
+++ 1360/1360B.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define FOR(i, a, b) for (i = a; i < b; i++)
+#define FastIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef priority_queue<ll, vector<ll>, greater<ll>> minheap;
+typedef priority_queue<ll> maxheap;
+int dxrow[] = {-1, 0, 0, 1, 1, 1, -1, -1}; //first 4 straight last 4 diagonals
+int dycol[] = {0, -1, 1, 0, 1, -1, -1, 1};
+void printV(vector<ll> v)
+{
+    for (ll i = 0; i < v.size(); i++)
+        cout << v[i] << "\n";
+}
+int modexp(ll A, ll B, ll C)
+{
+    if (A == 0)
+        return 0;
+    if (B == 0)
+        return 1;
+    ll y;
+    if (B % 2 == 0)
+    {
+        y = modexp(A, B / 2, C);
+        y = (y * y) % C;
+    }
+    else
+    {
+        y = A % C;
+        y = (y * modexp(A, B - 1, C) % C) % C;
+    }
+
+    return (ll)((y + C) % C);
+}
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, i;
+        cin >> n;
+        ll a[n];
+        FOR(i, 0, n)
+        {
+            cin >> a[i];
+        }
+        sort(a, a + n);
+        ll ans = LLONG_MAX;
+        FOR(i, 1, n)
+        {
+            ans = min(ans, abs(a[i] - a[i - 1]));
+        }
+        cout << ans << '\n';
+    }
+}
